ae6a545a4bdda40c4245fbfe2b388dd54b9b2dba
joavegaco <joavegaco311@gmail.com>
2021-09-13 07:10:06 -0500

    a

---
+++ practicaFunciones.py
@@ -0,0 +1,17 @@
+def AreaRectangulo (a,b):
+    area = a*b
+    return area
+
+def Pizza(sabor):
+    print(f"Quiero una pizza de {sabor}")
+
+a = input("Introduzca sabor: ")
+b= input("Introduzca otro sabor: ")
+
+Pizza(a)
+Pizza(b)
+
+rectangulo1 = AreaRectangulo(4,2)
+
+print(rectangulo1)
+
